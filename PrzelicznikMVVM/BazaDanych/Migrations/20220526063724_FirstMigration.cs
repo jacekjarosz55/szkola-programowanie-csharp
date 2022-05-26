b18e0cea2014156eebf0da42ea44e2265ba6555c
Uczen <Uczen@ST001-S311>
2022-05-26 08:39:09 +0200

    dodanie converter.db + solucji

---
+++ PrzelicznikMVVM/BazaDanych/Migrations/20220526063724_FirstMigration.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrzelicznikMVVM.BazaDanych.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnitTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnitTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CharRepresentation = table.Column<string>(type: "TEXT", nullable: true),
+                    TypeId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Units_UnitTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "UnitTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Converters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UnitFromId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UnitToId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Converters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Converters_Units_UnitFromId",
+                        column: x => x.UnitFromId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Converters_Units_UnitToId",
+                        column: x => x.UnitToId,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Converters_UnitFromId",
+                table: "Converters",
+                column: "UnitFromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Converters_UnitToId",
+                table: "Converters",
+                column: "UnitToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Units_TypeId",
+                table: "Units",
+                column: "TypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Converters");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+
+            migrationBuilder.DropTable(
+                name: "UnitTypes");
+        }
+    }
+}
